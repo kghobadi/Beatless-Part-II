562de9b9b08669de04fc4cfc45650263ce448bb9
Unknown <ksg304@nyu.edu>
2017-11-30 16:32:03 -0500

    some visitor changes

--- Assets/ProofOfConcept/Scripts/Systems/Visitor.cs
+++ Assets/ProofOfConcept/Scripts/Systems/Visitor.cs
@@ -65,6 +65,7 @@ public class Visitor : MonoBehaviour
         if (randy == 2)
             targetPlants = LookingForPlant.TRIANGLE;
 
+        desiredPlantAmount += Random.Range(0, 3);
 
         if (targetPlants == LookingForPlant.CIRCLE)
         {
